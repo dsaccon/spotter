c0f6298cc559bb8f2cb2950e878a2e45a5b377bd
ds <deerff@gmail.com>
2020-04-12 13:45:57 -0700

    add check for if instance id arg passed in

--- login.sh
+++ login.sh
@@ -1,4 +1,11 @@
 # Pass in instance ID as first arg when running script
+
+if [ -z "$1" ]
+then
+    echo "No arg passed in. You need to passed in one arg for instance-ID, eg './login i-0c24488afbcf2dd2f'"
+    exit 1
+fi
+
 export INST_ID=$1
 
 IP_ADDR=$(aws ec2 describe-instances --region us-west-2 --instance-ids $INST_ID --query "Reservations[*].Instances[*].PublicDnsName" --output=text)
