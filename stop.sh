7c4c28b7ca50a1f8c2d82b443287e46885bb5819
ds <deerff@gmail.com>
2020-04-12 16:34:26 -0700

    add check for if instance id arg passed in

--- stop.sh
+++ stop.sh
@@ -1,4 +1,11 @@
 # Pass in instance ID as first arg when running script
+
+if [ -z "$1" ]
+then
+    echo "No arg passed in. You need to passed in one arg for instance-ID, eg './stop i-0c24488afbcf2dd2f'"
+    exit 1
+fi
+
 export INST_ID=$1
 
-aws ec2 stop-instances --instance-ids $INST_ID
+aws ec2 stop-instances --instance-ids $INST_ID 2> /dev/null
