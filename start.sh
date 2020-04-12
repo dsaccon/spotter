043cfb6fe347ee9fd4c879fbdd627b3aa7571a8f
sirdavealot <david.g.saccon@gmail.com>
2020-04-11 18:03:51 -0700

    add more automation

--- start.sh
+++ start.sh
@@ -1,1 +1,10 @@
-aws ec2 start-instances --instance-ids i-0c22477afbdf2ed0f
+# Pass in instance ID as first arg when running script
+export INST_ID=$1
+aws ec2 start-instances --instance-ids $INST_ID
+
+echo Waiting for spot instance to start up...
+aws ec2 wait instance-running --instance-ids $INST_ID
+
+IP_ADDR=$(aws ec2 describe-instances --region us-west-2 --instance-ids $INST_ID --query "Reservations[*].Instances[*].PublicDnsName" --output=text)
+
+ssh -i ~/atg_oregon.pem -o StrictHostKeyChecking=no ubuntu@$IP_ADDR
