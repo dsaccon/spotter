cfea99091ec3af5dd9cf2131f8c301b2d93fc742
ds <deerff@gmail.com>
2020-04-14 17:42:54 -0700

    remove spot-price arg to use default (max price equals on-demand price)

--- req.sh
+++ req.sh
@@ -1,6 +1,6 @@
 # Request to start spot instance according to settings in spec.json, and print instance ID when it is up
 
-REQ_ID=$(aws ec2 request-spot-instances --spot-price "0.0226" --instance-count 1 --type "persistent" --launch-specification file://spec.json --output="text" --query="SpotInstanceRequests[*].SpotInstanceRequestId")
+REQ_ID=$(aws ec2 request-spot-instances --instance-count 1 --type "persistent" --launch-specification file://spec.json --output="text" --query="SpotInstanceRequests[*].SpotInstanceRequestId")
 
 aws ec2 wait spot-instance-request-fulfilled --spot-instance-request-ids $REQ_ID
 
