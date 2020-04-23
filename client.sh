d511d786dba5b9a712fafe827d79fc58d29905a7
ds <deerff@gmail.com>
2020-04-23 12:43:38 -0700

    add install()/uninstall()/start() funcs, add --start runtime arg

--- client.sh
+++ client.sh
@@ -1,6 +1,75 @@
 # Desktop client. Uses multipass to spin up an Ubuntu VM to act as a jump server for logging into the server
 # AWS CLI and necessary scripts are installed on the server to enable dynamically calling for instance ID, instance DNS address etc..
 
+install() {
+    echo ''
+    echo 'Installing.. The process will take a few mins'
+    echo ''
+    if [ "$1" == 'Darwin' ]; then
+        brew update
+        brew cask install multipass
+        sleep 10
+    elif [ "$1" == 'Linux' ]; then
+        sudo snap install multipass --classic
+        sleep 5
+    fi
+}
+
+uninstall() {
+    echo ''
+    echo 'Removing existing installation...'
+    echo ''
+    if [ "$1" == 'Darwin' ]; then
+        brew update
+        brew cask uninstall multipass
+        sleep 5
+    elif [ "$1" == 'Linux' ]; then
+        sudo snap remove multipass
+        sleep 5
+    fi
+}
+
+start() {
+    # Takes one argument, for name of VM (e.g. 'spot-jump-923')
+
+    # Check if instance is up. If not, start it
+    str=$(multipass exec $1 -- aws ec2 describe-instances --filters "Name=tag:Name,Values=Backtesting_spot" --output=text --query="Reservations[*].Instances[*].State")
+    str=`echo $str | tr " " :`
+    STATE=${str##*:}
+
+    OS=`uname`
+    KEYFILE='keys.txt'
+    if [ "$OS" == 'Darwin' ]; then
+        KB_PATH='/Volumes/Keybase/team/atg_and_obt/'
+    elif [ "$OS" == 'Linux' ]; then
+        KB_PATH='/keybase/team/atg_and_obt/'
+    fi
+    _KEY=$(grep -A2 '# OBT AWS' "$KB_PATH$KEYFILE" | tail -1)
+    _SECRET=$(grep -A3 '# OBT AWS' "$KB_PATH$KEYFILE" | tail -1)
+    KEY="${_KEY#*=}"
+    SECRET="${_SECRET#*=}"
+
+    INST_ID=$(multipass exec $1 -- aws ec2 describe-instances --filters "Name=tag:Name,Values=Backtesting_spot" --output=text --query="Reservations[*].Instances[*].InstanceId")
+    if [ "$STATE" == stopped ]; then
+        echo 'Instance not currently running. Starting it.. May take 1-2 mins'
+        multipass exec $1 -- ./spotter/start.sh $INST_ID $KEY $SECRET
+#        INST_ID=$(multipass exec $VM_NAME -- aws ec2 describe-instances --filters "Name=tag:Name,Values=Backtesting_spot" --output=text --query="Reservations[*].Instances[*].InstanceId")
+#        IP_ADDR=$(multipass exec $VM_NAME -- aws ec2 describe-instances --region us-west-2 --instance-ids $INST_ID --query "Reservations[*].Instances[*].PublicDnsName" --output=text)
+#        echo ''
+#        echo 'Instance public DNS: '$IP_ADDR
+#        echo ''
+    elif [ "$STATE" == stopping ]; then
+        echo 'Instance not fully stopped. Please wait a couple minutes for it to completely shut down'
+        exit 1
+    elif [ "$STATE" == running ]; then
+        echo 'Instance already running'
+    fi
+
+}
+
+
+#
+### Main ###
 #
 RANGE=999
 num=$RANDOM
@@ -8,20 +77,31 @@ let "num %= $RANGE"
 VM_NAME="spot-jump-"$num
 
 if [ "$1" == --setup ]; then
-    echo 'Installing.. The process will take 5-10 mins'
-    echo ''
 
     # Install multipass if not already installed
     str=`command -v multipass`
     OS=`uname`
     if [ "$str" == '' ]; then
-        if [ "$OS" == 'Darwin' ]; then
-            brew update
-            brew cask install multipass
-            sleep 10
-        elif [ "$OS" == 'Linux' ]; then
-            sudo snap install multipass --classic
-            sleep 5
+        install $OS
+#        echo 'Installing.. The process will take a few mins'
+#        echo ''
+#        if [ "$OS" == 'Darwin' ]; then
+#            brew update
+#            brew cask install multipass
+#            sleep 10
+#        elif [ "$OS" == 'Linux' ]; then
+#            sudo snap install multipass --classic
+#            sleep 5
+#        fi
+    else
+        read -p 'The app is already installed on your PC. Do you want to reinstall it with the latest version? (Y/N) ' conf
+        if [[ $conf == Y* ]] || [[ $conf == y* ]]; then
+            uninstall $OS
+            install $OS
+        else
+            echo 'Exiting program...'
+            echo ''
+            exit 1
         fi
     fi
 
@@ -73,37 +153,45 @@ if [ "$1" == --setup ]; then
     STR="$VM_NAME -- chmod 600 /home/ubuntu/$PEMFILE"
     multipass exec $STR
     echo ''
-    echo 'Client successfully set up. You can now log in (i.e. run with --login arg)'
+    echo 'Client successfully set up. You can now log in to the spot instance (i.e. run client with --login arg)'
+    echo ''
+
+elif [ "$1" == --start ]; then
+    str=$(multipass ls --format csv | tail -1)
+    VM_NAME="${str%%,*}"
+
+    start $VM_NAME
 
 elif [ "$1" == --login ]; then
     str=$(multipass ls --format csv | tail -1)
     VM_NAME="${str%%,*}"
 
-    # Check if instance is up. If not, start it
-    str=$(multipass exec $VM_NAME -- aws ec2 describe-instances --filters "Name=tag:Name,Values=Backtesting_spot" --output=text --query="Reservations[*].Instances[*].State")
-    str=`echo $str | tr " " :`
-    STATE=${str##*:}
-
-    OS=`uname`
-    KEYFILE='keys.txt'
-    if [ "$OS" == 'Darwin' ]; then
-        KB_PATH='/Volumes/Keybase/team/atg_and_obt/'
-    elif [ "$OS" == 'Linux' ]; then
-        KB_PATH='/keybase/team/atg_and_obt/'
-    fi
-    _KEY=$(grep -A2 '# OBT AWS' "$KB_PATH$KEYFILE" | tail -1)
-    _SECRET=$(grep -A3 '# OBT AWS' "$KB_PATH$KEYFILE" | tail -1)
-    KEY="${_KEY#*=}"
-    SECRET="${_SECRET#*=}"
-
-    INST_ID=$(multipass exec $VM_NAME -- aws ec2 describe-instances --filters "Name=tag:Name,Values=Backtesting_spot" --output=text --query="Reservations[*].Instances[*].InstanceId")
-    if [ "$STATE" == stopped ]; then
-        echo 'Instance not currently running. Starting it.. May take 1-2 mins'
-        multipass exec $VM_NAME -- ./spotter/start.sh $INST_ID $KEY $SECRET
-    elif [ "$STATE" == stopping ]; then
-        echo 'Instance not fully stopped. Please wait a couple minutes for it to completely shut down'
-        exit 1
-    fi
+    start $VM_NAME
+#    # Check if instance is up. If not, start it
+#    str=$(multipass exec $VM_NAME -- aws ec2 describe-instances --filters "Name=tag:Name,Values=Backtesting_spot" --output=text --query="Reservations[*].Instances[*].State")
+#    str=`echo $str | tr " " :`
+#    STATE=${str##*:}
+#
+#    OS=`uname`
+#    KEYFILE='keys.txt'
+#    if [ "$OS" == 'Darwin' ]; then
+#        KB_PATH='/Volumes/Keybase/team/atg_and_obt/'
+#    elif [ "$OS" == 'Linux' ]; then
+#        KB_PATH='/keybase/team/atg_and_obt/'
+#    fi
+#    _KEY=$(grep -A2 '# OBT AWS' "$KB_PATH$KEYFILE" | tail -1)
+#    _SECRET=$(grep -A3 '# OBT AWS' "$KB_PATH$KEYFILE" | tail -1)
+#    KEY="${_KEY#*=}"
+#    SECRET="${_SECRET#*=}"
+#
+#    INST_ID=$(multipass exec $VM_NAME -- aws ec2 describe-instances --filters "Name=tag:Name,Values=Backtesting_spot" --output=text --query="Reservations[*].Instances[*].InstanceId")
+#    if [ "$STATE" == stopped ]; then
+#        echo 'Instance not currently running. Starting it.. May take 1-2 mins'
+#        multipass exec $VM_NAME -- ./spotter/start.sh $INST_ID $KEY $SECRET
+#    elif [ "$STATE" == stopping ]; then
+#        echo 'Instance not fully stopped. Please wait a couple minutes for it to completely shut down'
+#        exit 1
+#    fi
 
     multipass exec $VM_NAME -- aws ec2 describe-instances --region us-west-2 --instance-ids $INST_ID --output=text --query "Reservations[*].Instances[*].PublicDnsName"
     echo 'SSHing into instance.. Type <exit> anytime to return to your regular shell'
@@ -123,7 +211,7 @@ elif [ "$1" == --getaddr ]; then
     VM_NAME="${str%%,*}"
     INST_ID=$(multipass exec $VM_NAME -- aws ec2 describe-instances --filters "Name=tag:Name,Values=Backtesting_spot" --output=text --query="Reservations[*].Instances[*].InstanceId")
     IP_ADDR=$(multipass exec $VM_NAME -- aws ec2 describe-instances --region us-west-2 --instance-ids $INST_ID --query "Reservations[*].Instances[*].PublicDnsName" --output=text)
-    echo 'Public DNS: '$IP_ADDR
+    echo 'Instance public DNS: '$IP_ADDR
 
 elif [ "$1" == --status ]; then
     str=$(multipass ls --format csv | tail -1)
@@ -134,12 +222,15 @@ elif [ "$1" == --status ]; then
     echo $STATE
 
 else
-    echo 'This tool automates logins to spot instances that change over time'
-    echo 'Run the script with the appropriate argument below. If this is your first time, use --setup:'
-    echo "    --setup    (One-time initial setup, ie './client --setup')"
+    echo ''
+    echo 'This tool automates logins to dynamic spot instances'
+    echo 'Run with the appropriate argument below. If this is your first time, or you want to upgrade to the latest version, use --setup:'
+    echo "    --setup    (Install app on your PC, or reinstall from scratch if already installed. Ie, './client --setup')"
+    echo '    --start    (Start spot instance)'
     echo '    --login    (Login to spot instance)'
     echo '    --stop     (Stop spot instance when you are finished with it)'
     echo '    --getaddr  (Get public DNS address of running instance)'
     echo '    --update   (Update to latest version of this client and supporting libraries. WORK IN PROGRESS)'
     echo '    --status   (Check if the instance is up/down)'
+    echo ''
 fi
